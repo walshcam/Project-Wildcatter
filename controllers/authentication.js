5bb9b260650c2f027dd74803686ac1c419a19d21
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-31 10:08:23 -0400

    Successfully send new users to database - add functionality to make user provide username and password

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,4 @@
-const User = require('../models')
+const db = require('../models')
 
 exports.signup = function(req, res, err) {
     //Obtain email and password from post request
@@ -6,15 +6,29 @@ exports.signup = function(req, res, err) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if (!email || !password) {
+        return res.status(422).send({ error: 'You must provide an email and password'});
+    }
+
     // See if a user with a given email exists
-    User.findOne({ email: email }, function(err, existingUser) {
-        
+    db.User.findOne({ email: email }, function(err, existingUser) {
+        //If a user with an email does exist, return an error
+        if(err) { return next(err); }
+
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use' })
+        }
+
+        //If a user with email does NOT exist, create and save user record
+        db.User.create({
+            email: email,
+            password: password
+        })
+        .then(function(){
+            res.json({ success: true });
+        })
+        .catch(function(err){
+            res.json(err);
+        });
     });
-
-
-    //If a user with an email does exist, return an error
-
-    //If a user with email does NOT exist, create and save user record
-
-    //Respond to request indicating the user was created
 }
